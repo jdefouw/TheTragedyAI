514470675def8084f09c51e2abf9d074d2c39466
jdefouw <joeldefouw@gmail.com>
2025-12-02 13:51:21 -0800

    Update all

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
