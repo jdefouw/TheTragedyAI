b4cb34fa78cbab4568a996622e70c99154edf921
jdefouw <joeldefouw@gmail.com>
2025-12-02 17:34:14 -0800

    Update all

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,27 +1,7 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  webpack: (config, { dir, isServer }) => {
-    // Get the absolute path to src directory relative to the config file
-    const srcPath = path.join(dir, 'src')
-    
-    // Ensure resolve and alias exist
-    if (!config.resolve) {
-      config.resolve = {}
-    }
-    
-    // Merge with existing aliases to avoid overwriting Next.js defaults
-    const existingAliases = config.resolve.alias || {}
-    config.resolve.alias = {
-      ...existingAliases,
-      '@': srcPath,
-    }
-    
-    return config
-  },
 }
 
 module.exports = nextConfig
